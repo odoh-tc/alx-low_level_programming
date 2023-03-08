91f45c6f534933a4f3358ba75e9607c8197376ab
Aximoo <tochukwuodoh03@gmail.com>
2023-03-08 07:20:52 -0800

    slenstr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strlen_recursion - print string length
- * @s: string
+ * _strlen_recursion - length of string
+ * @s:string
  * Return:int
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	int i = 0;
 
@@ -12,6 +12,7 @@ int _strlen_recursion(char *s);
 	{
 		i++;
 		i += _strlen_recursion(s + 1);
+
 	}
 	return (i);
 }
