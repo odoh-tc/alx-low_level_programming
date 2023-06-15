eb448cb9fcee53083eec093f95234b8c9efbd79f
Aximoo <tochukwuodoh03@gmail.com>
2023-06-15 11:10:16 -0700

    0-print_dlistint.c1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t m;
+	size_t x;
 
-	for (m = 0; h != NULL; m++)
+	for (x = 0; h != NULL; x++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (m);
+	return (x);
 }
